fc042133cca458f4bc6fe913d7abb158019e7179
Kuriakose <Manu.Kuriakose@bakerhughes.com>
2021-02-07 10:32:13 +0530

    First Commit

---
+++ leansp2/src/main/java/com/example/leansp2/Leansp2Application.java
@@ -0,0 +1,13 @@
+package com.example.leansp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leansp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leansp2Application.class, args);
+	}
+
+}
---
+++ leansp2/src/test/java/com/example/leansp2/Leansp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leansp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leansp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
